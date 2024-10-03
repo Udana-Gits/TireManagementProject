691144c094e3547d543c1e89fdaedbe70c7f5fb8
janith <janithjayashan018@gmail.com>
2024-10-03 10:11:32 +0530

    Update LogIn.jsx

--- src/components/auth/LogIn.jsx
+++ src/components/auth/LogIn.jsx
@@ -153,7 +153,6 @@ const LogIn = () => {
             Forgot Password?
           </button>
           </div>
-          <br /><br />
           <button type="submit" className="loginbutton">
             Log In
           </button>
